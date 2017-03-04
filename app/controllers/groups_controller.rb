f71b5aec20bd8d131aba565e2a3ef60a1607aa7f
isaac <237957459@qq.com>
2017-03-04 21:51:37 +0800

    user before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,7 +10,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    find_group_and_check_permission
   end
 
   def new
@@ -28,8 +27,6 @@ class GroupsController < ApplicationController
 end
 
   def update
-    find_group_and_check_permission
-
     if @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
   else
@@ -38,8 +35,6 @@ end
 end
 
   def destroy
-    find_group_and_check_permission
-
     @group.destroy
     flash[:alter] = "Group deleted"
     redirect_to groups_path
